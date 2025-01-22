be9fa7a3a5325ed49d6032a33868ffcfe7a8c05f
bbioren <bbioren@uw.edu>
2025-01-22 08:23:48 +0000

    Deploying to gh-pages from @ bbioren/bbioren.github.io@7230a9875352c0840c8e79ee6891626596f1e589 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,14 +492,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%62%69%6F%72%65%6E@%75%77.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/bbioren", "_blank");
+        },
+      },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/ben_bioren", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/ben-bioren", "_blank");
         },
       },{
         id: 'social-rss',
@@ -509,11 +523,11 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-youtube',
+        title: 'YouTube',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://youtube.com/@benbioren", "_blank");
         },
       },{
         id: 'social-custom_social',
