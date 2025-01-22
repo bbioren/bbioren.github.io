e882d36c09a22dcdb0bbc381092e4cf1dd4c73d7
bbioren <bbioren@uw.edu>
2025-01-22 20:56:23 +0000

    Deploying to gh-pages from @ bbioren/bbioren.github.io@e0a0a5310cd008d48d0774d05ceb174b23af4774 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "dropdown-publications",
+  },{id: "dropdown-publications",
               title: "publications",
               description: "",
               section: "Dropdown",
