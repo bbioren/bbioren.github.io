3d74a5ead1e9d43c6115fba881c922a137dbc0a5
bbioren <bbioren@uw.edu>
2025-01-22 22:07:38 +0000

    Deploying to gh-pages from @ bbioren/bbioren.github.io@c5b0f6cc2b094c2b1ed3fb796b2fe21164f0eb22 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
-          },
-        },{id: "post-a-post-with-image-galleries",
+  },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
